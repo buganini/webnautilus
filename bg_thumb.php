f8689cadf3b6ae31097e404052be4c8812b5e4d5
Charlie <root@www.chops.ntct.edu.tw>
2011-03-29 22:15:48 +0800

    document conversion is working now!

--- bg_thumb.php
+++ bg_thumb.php
@@ -42,24 +42,34 @@ function doThumb($job){
 				if(!file_exists($CFG['cachedir'].$hash.'.mp4')){
 #					$cmd1=$CFG['mencoder'].' '.escapeshellarg(r($rootdir.$file)).' -of lavf -lavfopts format=mp4 -sws 9 -af volnorm -srate 48000 -channels 2 -vf-add scale=480:272,harddup -oac faac -faacopts br=96:mpeg=4:object=2:raw -ovc x264 -ffourcc H264 -x264encopts crf=22:threads=2:level_idc=30:bframes=3:frameref=2:global_header:partitions=all -o '.escapeshellarg($CFG['tempdir'].$thash.'.mp4');
 #					$cmd1=$CFG['ffmpeg'].' -y -i '.escapeshellarg(r($rootdir.$file)).' -acodec libfaac -ar 44100 -ab 96k -vcodec libx264 -level 41 -crf 25 -bufsize 20000k -maxrate 25000k -g 250 -r 20 -s 1280x720 '.escapeshellarg($CFG['tempdir'].$thash.'.mp4');
-#					echo $cmd1;
+#					$cmd1=$CFG['mencoder'].' -of lavf -lavfopts format=mp4 -oac lavc -ovc lavc -lavcopts aglobal=1:vglobal=1:acodec=libfaac:abitrate=128:vcodec=mpeg4:keyint=25 -ofps 25 -af lavcresample=44100 -vf harddup,scale=640:480 -mc 0 -noskip '.escapeshellarg(r($rootdir.$file)).' -o '.escapeshellarg($CFG['tempdir'].$thash.'.mp4');
+					$cmd1=$CFG['ffmpeg'].' -y -i '.escapeshellarg(r($rootdir.$file)).' -vcodec libx264 -qmin 10 -qmax 51 -qdiff 4 -me_range 16 -keyint_min 25 -qcomp 0.6 -b 400K '.escapeshellarg($CFG['tempdir'].$thash.'.mp4');
+echo $cmd1."\n\n";
 #					exe($cmd1);
 					rmtry($CFG['cachedir'].$hash.'.mp4');
 					rmtry($CFG['cachedir'].$hash.'_L.jpg');
 					rmtry($CFG['cachedir'].$hash.'.jpg');
 					$cmd2=$CFG['yamdi'].' -i '.escapeshellarg($CFG['tempdir'].$thash.'.mp4').' -o '.escapeshellarg($CFG['cachedir'].$hash.'.mp4');
+echo $cmd2."\n\n";
 					exe($cmd2);
-					uunlink($CFG['tempdir'].$thash.'.mp4');
+					if(!file_exists($CFG['cachedir'].$hash.'.mp4')){
+						copy($CFG['tempdir'].$thash.'.mp4',$CFG['cachedir'].$hash.'.mp4');
+					}
+#					uunlink($CFG['tempdir'].$thash.'.mp4');
 				}
 				if(!file_exists($CFG['cachedir'].$hash.'_L.jpg')){
-					$info=exe($CFG['mplayer'].' -identify -nosound -vc dummy -vo null '.escapeshellarg($CFG['cachedir'].$hash.'.mp4'));
+					$cmd3=$CFG['mplayer'].' -identify -nosound -vc dummy -vo null '.escapeshellarg($CFG['cachedir'].$hash.'.mp4');
+echo $cmd3."\n\n";
+					$info=exe($cmd3);
 					preg_match('/ID_LENGTH=([0-9\\.]+)/s',$info,$len);
 					$len=$len[1];
-					$cmd3=$CFG['ffmpeg'].' -i '.escapeshellarg($CFG['cachedir'].$hash.'.mp4').' -y -f image2 -ss '.($len/2).' -t 0.001 -s 400x326 '.escapeshellarg($CFG['cachedir'].$hash.'_L.jpg');
-					exe($cmd3);
+					$cmd4=$CFG['ffmpeg'].' -i '.escapeshellarg($CFG['cachedir'].$hash.'.mp4').' -y -f image2 -ss '.($len/2).' -t 0.001 -s 960x720 '.escapeshellarg($CFG['cachedir'].$hash.'_L.jpg');
+echo $cmd4."\n\n";
+					exe($cmd4);
 				}
-				$cmd4=$CFG['imagemagick_convert'].' -quality 70 -geometry '.$size.' '.escapeshellarg($CFG['cachedir'].$hash.'_L.jpg').' '.escapeshellarg($CFG['cachedir'].$hash.'_'.$size.'.jpg');
-				exe($cmd4);
+				$cmd5=$CFG['imagemagick_convert'].' -quality 70 -geometry '.$size.' '.escapeshellarg($CFG['cachedir'].$hash.'_L.jpg').' '.escapeshellarg($CFG['cachedir'].$hash.'_'.$size.'.jpg');
+echo $cmd5."\n\n";
+				exe($cmd5);
 			myunlock($thash);
 		}
 	}elseif(isimage($file)){
@@ -93,6 +103,7 @@ function doThumb($job){
 						$cmd=$CFG['unoconv'].' -f pdf '.escapeshellarg($tfile);
 						exe($cmd);
 					}
+					copy($pdf,$CFG['cachedir'].$hash.'.pdf');
 					$cmd=$CFG['ghostscript'].' -dNOPAUSE -dBATCH -dFirstPage=1 -dLastPage=1 -sDEVICE=bmp16 -sOutputFile='.$CFG['tempdir'].$thash.'.bmp '.escapeshellarg($pdf);
 					#$cmd=$CFG['imagemagick_convert'].' '.escapeshellarg($pdf).' '.escapeshellarg($CFG['tempdir'].$thash.'.jpg');
 					exe($cmd);
--- document.php
+++ document.php
@@ -7,6 +7,6 @@ if(($file=safepath($rootdir,$_GET['file']))===false){
 $hash=$_GET['base'].'/'.mkhash($rootdir.$file);
 
 if(ufile_exists($CFG['cachedir'].$hash.'.pdf')){
-	redirect('http://docs.google.com/viewer?url='.urlencode($CFG['url'].$CFG['cachedir'].$hash.'.pdf'));
+	redirect('http://docs.google.com/viewer?url='.urlencode($CFG['url'].$CFG['cacheurl'].$hash.'.pdf'));
 }
 ?>
