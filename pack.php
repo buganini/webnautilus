ff69c4f1730f60c383ae3c66ea9715a3e67ae711
Charlie <root@www.chops.ntct.edu.tw>
2011-04-05 03:13:13 +0800

    temporary approach for packing

--- pack.php
+++ pack.php
@@ -16,11 +16,12 @@ $apath=explode('/',$path);
 $bdir=array_pop($apath);
 chdir(r(implode('/',$apath)));
 
-$zfile=$bdir.'-'.$thash.'.tar';
+$zfile=$bdir.'.7z';
 
-header('Content-type: application/x-tar');
-header('Content-Disposition: attachment; filename="'.preg_replace('![\\/?]!i','',$zfile).'"');
-$cmd=$CFG['sevenzip'].' a -tTar -so dummy '.escapeshellarg(r($bdir));
+#header('Content-type: application/x-zip-compressed');
+#header('Content-Disposition: attachment; filename*=utf-8"'.preg_replace('![\\/?]!i','',$zfile).'"');
+$cmd=$CFG['sevenzip'].' a -t7z '.escapeshellarg($CFG['cachedir'].$zfile).' '.escapeshellarg(r($bdir));
 exe($cmd);
-passthru($cmd);
+redirect($CFG['cacheurl'].$zfile);
+#passthru($cmd);
 ?>
